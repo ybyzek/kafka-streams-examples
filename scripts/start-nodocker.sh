d5a7c91137fb56788dc6760bf774bee5d91a2c94
Yeva Byzek <yeva@confluent.io>
2018-03-20 23:10:09 -0400

    Optimizing for confluent-cli

--- scripts/start-nodocker.sh
+++ scripts/start-nodocker.sh
@@ -3,7 +3,11 @@
 confluent destroy
 jps | grep KafkaMusicExampleDriver | awk '{print $1;}' | xargs kill -9
 
+[[ -d $CONFLUENT_HOME/ui ]] || mkdir -p "$CONFLUENT_HOME/ui"
+[[ -f "$CONFLUENT_HOME/ui/ksql-experimental-ui-0.1.war" ]] || wget --directory-prefix="$CONFLUENT_HOME/ui" https://s3.amazonaws.com/ksql-experimental-ui/ksql-experimental-ui-0.1.war
+
 echo "auto.offset.reset=earliest" >> $CONFLUENT_HOME/etc/ksql/ksql-server.properties
+echo "ksql.ui.enabled=false" >> $CONFLUENT_HOME/etc/ksql/ksql-server.properties
 confluent start ksql-server
 
 #mvn clean package -DskipTests
